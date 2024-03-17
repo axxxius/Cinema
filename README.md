24d785079604dc6e69c631239a0e836a5fe07763
Ilya Konoplitsky <96144903+IlyaKonoplitsky@users.noreply.github.com>
2024-03-17 23:56:30 +0500

    :comet: client configuration

---
+++ README.md
@@ -0,0 +1,9 @@
+# :popcorn: Cinema
+
+## Cinema - это веб сервис для покупки билетов в кинотеатр.
+
+**Конфигурация проекта:**
+
+![React](https://img.shields.io/badge/React-61DAFB.svg?style=for-the-badge&logo=React&logoColor=black)
+
+![Nest](https://img.shields.io/badge/NestJS-E0234E.svg?style=for-the-badge&logo=NestJS&logoColor=white)
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import * as path from "path";
+import { defineConfig } from "vite";
+
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@api": path.resolve(__dirname, "./src/api/api"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@router": path.resolve(__dirname, "./src/router"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@utils/api": path.resolve(__dirname, "./src/utils/api"),
+    },
+  },
+});
